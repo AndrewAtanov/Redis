4a57172353ad626957c231879915a5890444d918
Andrew <toretto.gtr@gmail.com>
2015-10-30 15:08:10 +0300

    RESP added

--- redis_server.cpp
+++ redis_server.cpp
@@ -19,70 +19,69 @@ Redis_server::Redis_server(int port, redis_api* _r) {
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     bind(listener, (struct sockaddr *)&addr, sizeof(addr));
-    listen(listener, 1);
+    listen(listener, 2);
 }
 
 void Redis_server::get_requests() {
     int sock = accept(listener, nullptr, nullptr);
-    std::string request;
-    while(full_req_read(sock, request)) {
-        std::stringstream in(request);
-        std::string command;
-        in >> command;
-        if (command == "SET") {
-            std::string key, val;
-            in >> key >> val;
-            redis->set(key, val);
-        } else if (command == "GET") {
-            std::string key;
-            in >> key;
-            std::string ans = redis->get(key);
-            full_req_write(sock, ans);
-        } else if (command == "show") {
-            std::stringstream stream;
-            redis->show_map(stream);
-            stream.seekp(0);
-            std::string str;
-            while (std::getline(stream, str)) {
-                full_req_write(sock, str);
-            }
-            str = "";
-            full_req_write(sock, str);
-        } else if (command == "exit") {
+    while (1) {
+        std::string str_req;
+        if (!full_req_read(sock, str_req))
+            break;
+        std::vector<std::string> req_arr;
+//        std::cout << str_req << std::endl;
+            str_to_arr(str_req, req_arr);
+        req_arr[0];
+        if (req_arr[0] == "SET") {
+            std::string ans = redis->set(req_arr[1], req_arr[2]);
+            std::string resp;
+            string_to_req(ans, resp);
+            full_req_write(sock, resp);
+        } else if (req_arr[0] == "GET") {
+            std::string ans = redis->get(req_arr[1]);
+            std::string resp;
+            string_to_req(ans, resp);
+            full_req_write(sock, resp);
+        } else if (req_arr[0] == "show") {
+            std::string ans = "NO";
+            std::string resp;
+            string_to_req(ans, resp);
+            full_req_write(sock, resp);
+        } else if (req_arr[0] == "exit") {
             break;
         }
     }
     close(sock);
 }
 
-bool Redis_server::full_req_read(int sd, std::string &req) {
-    char* buff = new char[4];
-    int rd_size = recv(sd, buff, 4, 0);
-    if (rd_size <= 0)
-        return false;
-    while ((rd_size += recv(sd, buff + rd_size, 4 - rd_size, 0)) != 4) {}
-    int need_size = *reinterpret_cast<int *>(buff);
-    char *big_buff = new char[need_size];
-    rd_size = recv(sd, big_buff, need_size, 0);
-    while (rd_size != need_size) {
-        rd_size += recv(sd, big_buff + rd_size, need_size - rd_size, 0);
-    }
+//bool Redis_server::full_req_read(int sd, std::string &req) {
+//    char* buff = new char[4];
+//    int rd_size = recv(sd, buff, 4, 0);
+//    if (rd_size <= 0)
+//        return false;
+//    while ((rd_size += recv(sd, buff + rd_size, 4 - rd_size, 0)) != 4) {}
+//    int need_size = *reinterpret_cast<int *>(buff);
+//    char *big_buff = new char[need_size];
+//    rd_size = recv(sd, big_buff, need_size, 0);
+//    while (rd_size != need_size) {
+//        rd_size += recv(sd, big_buff + rd_size, need_size - rd_size, 0);
+//    }
 
-    req = std::string(big_buff);
-    req = req.substr(0, need_size) + "\0";
+//    req = std::string(big_buff);
+//    req = req.substr(0, need_size) + "\0";
 
-    return true;
-}
+//    return true;
+//}
 
-bool Redis_server::full_req_write(int sd, std::string &req) {
-    int size = req.size();
-    int *size_ptr = &size;
-    const char *buff = reinterpret_cast<const char *>(size_ptr);
-    int send_size = send(sd, buff, 4, 0);
-    while ((send_size += send(sd, buff + send_size, 4 - send_size, 0)) != 4) {}
-    buff = req.c_str();
-    send_size = send(sd, buff, req.size(), 0);
-    while ((send_size += send(sd, buff + send_size, req.size() - send_size, 0)) != req.size()) {}
+//bool Redis_server::full_req_write(int sd, std::string &req) {
+//    int size = req.size();
+//    int *size_ptr = &size;
+//    const char *buff = reinterpret_cast<const char *>(size_ptr);
+//    int send_size = send(sd, buff, 4, 0);
+//    while ((send_size += send(sd, buff + send_size, 4 - send_size, 0)) != 4) {}
+//    buff = req.c_str();
+//    send_size = send(sd, buff, req.size(), 0);
+//    while ((send_size += send(sd, buff + send_size, req.size() - send_size, 0)) != req.size()) {}
 
-    return true;
-}
+//    return true;
+//}
--- redis_server.h
+++ redis_server.h
@@ -2,6 +2,7 @@
 #define REDIS_SERVER_H
 
 #include "redis_api.h"
+#include "resp_api.h"
 
 #include <iostream>
 #include <sstream>
@@ -15,8 +16,8 @@ class Redis_server {
 private:
     int listener;
     redis_api *redis;
-    bool full_req_read(int sd, std::string& req);
-    bool full_req_write(int sd, std::string& req);
+//    bool full_req_read(int sd, std::string& req);
+//    bool full_req_write(int sd, std::string& req);
 public:
     Redis_server(int port, redis_api *_r);
     Redis_server();
